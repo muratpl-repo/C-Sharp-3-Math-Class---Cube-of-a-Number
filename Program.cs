106a5e1b9459cff03936e5e28355ab6bac6138cc
muratpala007 <muratpala007@gmail.com>
2022-04-19 15:55:21 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12DersMathSinifi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi = -16;
+
+            Console.WriteLine(sayi * sayi * sayi);
+            
+            
+            Console.WriteLine(5);
+
+            
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
